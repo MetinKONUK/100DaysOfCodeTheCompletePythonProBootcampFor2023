3e92909f3698ba183e730deddef033f26c64d4b8
Metin KONUK <metinkonuk@hotmail.com>
2022-06-28 14:26:53 +0300

    add interactive coding exercise 4

---
+++ fizz-buzz-exercise.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+
+for number in range(1, 101):
+    if not number % 15:
+        print("FizzBuzz")
+    elif not number % 5:
+        print("Buzz")
+    elif not number % 3:
+        print("Fizz")
+    else:
+        print(number)
