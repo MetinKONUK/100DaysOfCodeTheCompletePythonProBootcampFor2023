64dc74aeafad3ed324b7503706f23012d4f2eddd
Metin KONUK <metinkonuk@hotmail.com>
2022-06-28 18:58:38 +0300

    add challenge 1

---
+++ challenge-1.py
@@ -0,0 +1,15 @@
+def move():pass
+def turn_left():pass
+def turn_right():
+    for _ in range(0, 3):
+        turn_left()
+def draw_square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+draw_square()
