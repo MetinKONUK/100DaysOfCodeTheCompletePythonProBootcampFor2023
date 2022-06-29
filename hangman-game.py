f48e2052997e82d778768f15e2e6f131c4a6161c
Metin KONUK <metinkonuk@hotmail.com>
2022-06-29 16:39:37 +0300

    add final phase of the game

--- hangman-game.py
+++ hangman-game.py
@@ -1,21 +1,19 @@
 from random import choice
-
-words = ["beekeeper", "astral", "elephant" ,"basically"]
+from hangman_words import words
+from hangman_art import stages, logo
 word = choice(words)
 model = ['_' for _ in range(0, len(word))]
 rights = 7
 done = False
 left = len(word)
-
-#todo-1: - Use a while loop to let the user guess again.
-# The loop should only stop once the user has guessed all the letters in the chosen_word
-# and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
+print(logo)
 while True:
+    #Check if user has got all letters.
     if not left:
         print("You won!")
         break
     if not rights:
+        #If lives goes down to 0 then the game should stop and it should print "You lose."
         print("You lost!")
         break
     guess = input("Enter your letter guess: ").lower()
@@ -24,7 +22,10 @@ while True:
             if word[i] == guess:
                 model[i] = guess
                 left -= 1
-        print(model)
+        #Join all the elements in the list and turn it into a String.
+        print("".join(model))
     else:
+        #TODO-2: - If guess is not a letter in the chosen_word,
+        #Then reduce 'lives' by 1. 
         rights -= 1
-        print("Rights left: ", rights)
+        print(stages[rights])
