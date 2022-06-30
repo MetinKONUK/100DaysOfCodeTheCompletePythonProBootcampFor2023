ad238764ebf1c1c4d15f14eb9fc1bc8c613aecbc
Metin KONUK <metinkonuk@hotmail.com>
2022-06-30 14:39:20 +0300

    combine encrypt & decrypt functions into one function: caesar(@params)

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -3,36 +3,16 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def encrypt(text, shift):
-    encoded_text = ""
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+def caesar(text, shift, direction):
+    result_text = ""
     for letter in text:
         idx = alphabet.index(letter)
-        char = alphabet[(idx + shift) % len(alphabet)]
-        encoded_text += char
-    print(f"The encoded text is {encoded_text}")
-if direction == "encode":
-    encrypt(text, shift)
-
-
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-def decrypt(text, shift):
-    decoded_text = ""
-    for letter in text:
-        idx = alphabet.index(letter)
-        char = alphabet[(idx-shift+len(alphabet)) % len(alphabet)]
-        decoded_text += char
-    print(f"The decoded text is {decoded_text}")
-if direction == "decode":
-    decrypt(text, shift)
-        
+        char = alphabet[(idx + shift) % len(alphabet)]\
+        if direction == "encode"\
+        else alphabet[(idx-shift+len(alphabet)) % len(alphabet)]
+        result_text += char
+    print(f"The {direction}d text is {result_text}")
+caesar(text, shift, direction)
