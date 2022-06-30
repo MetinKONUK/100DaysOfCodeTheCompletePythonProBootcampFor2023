19558d76d7c01b82d4c8ad119b31366c506a86ad
Metin KONUK <metinkonuk@hotmail.com>
2022-06-30 14:30:00 +0300

    add caesar cipher part-2: decryption

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -4,22 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
 def encrypt(text, shift):
     encoded_text = ""
     for letter in text:
@@ -29,3 +13,26 @@ def encrypt(text, shift):
     print(f"The encoded text is {encoded_text}")
 if direction == "encode":
     encrypt(text, shift)
+
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+def decrypt(text, shift):
+    decoded_text = ""
+    for letter in text:
+        idx = alphabet.index(letter)
+        char = alphabet[(idx-shift+len(alphabet)) % len(alphabet)]
+        decoded_text += char
+    print(f"The decoded text is {decoded_text}")
+if direction == "decode":
+    decrypt(text, shift)
+        
