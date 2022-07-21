49f9255714fa5f77b696ec71f5402fbb25691d81
Metin KONUK <metinkonuk@hotmail.com>
2022-07-21 18:37:36 +0300

    fix escape char bug in art.py & call main function inside main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from art import text
 print(text)
 
 def main():
-    diff_level = input("Choose a difficulty level. Easy or Hard:").lower()
+    diff_level = input("Choose a difficulty level. Easy or Hard: ").lower()
     turns = 10 if diff_level == "easy" else 8
     number = randint(1, 100)
     while True:
-        guess = int(input("Enter a number in a range of [1, 100]"))
+        guess = int(input("Enter a number in a range of [1, 100]: "))
         if guess == number:
             print(f"Correct! Number was {number}.")
             break
@@ -20,3 +20,5 @@ def main():
         if not turns:
             print(f"Out of turns! The number was {number}.")
             break
+
+main()
