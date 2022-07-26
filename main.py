73bd6707b7d3222300a196d7ab56fd5e040ab059
Metin KONUK <metinkonuk@hotmail.com>
2022-07-26 14:41:43 +0300

    third challenge

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from turtle import Turtle, Screen
+from random import choice
 
 
-def dashed(pen: Turtle, space: float, length: float) -> None:
-    """Draw dashed line with turtle."""
-    pen.forward(length)
-    pen.penup()
-    pen.forward(space)
-    pen.pendown()
+def color_picker():
+    colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+               "SeaGreen"]
+    return choice(colours)
+
+
+def shape(pen: Turtle, n: int):
+    """Draws a regular shape with n sides."""
+    pen.color(color_picker())
+    for _ in range(n):
+        pen.forward(100)
+        pen.rt(360 // n)
 
 
 def main() -> None:
     pen = Turtle()
     pen.shape("turtle")
-    for _ in range(10):
-        dashed(pen, 5, 10)
+    for n in range(3, 10):
+        shape(pen, n)
     screen = Screen()
     screen.exitonclick()
 
 
 main()
-
