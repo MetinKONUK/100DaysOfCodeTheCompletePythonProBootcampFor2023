41ad7faf5adcd28403a0a12d30bce42a6687e26d
Metin KONUK <metinkonuk@hotmail.com>
2022-07-26 18:46:50 +0300

    fourth challenge

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ from turtle import Turtle, Screen
 from random import choice
 
 
-def color_picker():
+def color_picker() -> str:
     colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
                "SeaGreen"]
     return choice(colours)
 
 
-def shape(pen: Turtle, n: int):
-    """Draws a regular shape with n sides."""
-    pen.color(color_picker())
-    for _ in range(n):
-        pen.forward(100)
-        pen.rt(360 // n)
+def random_walk(pen: Turtle, directions: list, distance: float, width: int, speed: str, cycles: int) -> None:
+    pen.pensize(width)
+    pen.speed(speed)
+    while cycles:
+        pen.color(color_picker())
+        pen.setheading(choice(directions))
+        pen.forward(distance)
+        cycles -= 1
 
 
 def main() -> None:
     pen = Turtle()
-    pen.shape("turtle")
-    for n in range(3, 10):
-        shape(pen, n)
+    directions = [0, 90, 180, 270]
+    random_walk(pen, directions, 10, 4, "fastest", 200)
     screen = Screen()
     screen.exitonclick()
 
