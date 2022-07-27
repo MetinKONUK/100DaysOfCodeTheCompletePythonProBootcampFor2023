d95bd1cd5a32c4d4fb0c8c61e07e81837541fe32
Metin KONUK <metinkonuk@hotmail.com>
2022-07-27 18:05:20 +0300

    'imitating Hirst's painting with turtle' project

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 from random import choice
+from colorgram import extract
 
 
-def color_picker() -> str:
-    colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-               "SeaGreen"]
-    return choice(colours)
+def extract_colors(path: str, kind: int) -> list:
+    colors: list = extract(path, 100)
+    return [tuple(color.rgb) if kind == 0 else tuple(color.hsl) for color in colors][2:]
 
 
-def spirograph(pen: Turtle, offset: int, radius: float):
-    pen.speed("fastest")
-    circle_count = 360 // offset
-    for _ in range(circle_count):
-        pen.color(color_picker())
-        pen.circle(radius)
-        pen.setheading(pen.heading() + offset)
+def painting(pen: Turtle, colors: list) -> None:
+    pen.setheading(225)
+    pen.forward(325)
+    pen.setheading(0)
+    for __ in range(10):
+        for _ in range(10):
+            pen.dot(30, choice(colors))
+            pen.forward(50)
+        pen.setheading(90)
+        pen.forward(50)
+        pen.setheading(180)
+        pen.forward(500)
+        pen.setheading(0)
+    pen.hideturtle()
 
 
 def main() -> None:
     pen = Turtle()
-    spirograph(pen, 5, 100)
+    pen.speed("fastest")
+    pen.penup()
+
+    colormode(255)
+    colors = extract_colors("image.jpg", 0)
+    painting(pen, colors)
     screen = Screen()
     screen.exitonclick()
 
