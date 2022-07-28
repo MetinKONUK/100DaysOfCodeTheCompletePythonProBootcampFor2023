51d3a222dd8fb33dcb782b4dc444984e8586b07d
Metin KONUK <metinkonuk@hotmail.com>
2022-07-28 15:34:50 +0300

    turtle-race-guess  game project

--- main.py
+++ main.py
@@ -1,40 +1,68 @@
 from turtle import Turtle, Screen, colormode
-from functools import partial
-from random import choice
-from colorgram import extract
-index: int = 0
+from random import random, choice, randint
+global winner
 
 
-def clear_screen(pen: Turtle) -> None:
-    pen.clear()
+def draw_finish_line(pen: Turtle):
     pen.penup()
-    pen.home()
+    pen.goto(200, 500)
     pen.pendown()
+    pen.goto(200, -500)
+    pen.hideturtle()
 
 
-def switch_color(pen: Turtle, colors: list) -> None:
-    global index
-    pen.color(colors[index % 4])
-    index += 1
+def start_position(racer: Turtle, x: float, y: float) -> None:
+    racer.speed("slow")
+    racer.setposition(x, y)
+    racer.speed("slowest")
+
+
+def appearance(racer: Turtle, color: str):
+    racer.penup()
+    racer.shape("turtle")
+    racer.shapesize(2, 2, 1)
+    racer.color(color)
+
+
+def create_racers() -> list:
+    colors: list = ["red", "green", "blue", "yellow", "pink"]
+    racers: list = []
+    position: float = 120
+    color_index = 0
+    for _ in range(5):
+        racer = Turtle()
+        racer.speed("slowest")
+        appearance(racer, colors[color_index])
+        color_index += 1
+        start_position(racer, -250, position)
+        position -= 60
+        racers.append(racer)
+    return racers
+
+
+def check_winner(racers: list) -> bool:
+    for racer in racers:
+        if racer.xcor() >= 200:
+            global winner
+            winner = racer.color()
+            return False
+    return True
+
+
+def race(racers: list):
+    while check_winner(racers):
+        for racer in racers:
+            racer.forward(randint(15, 30))
 
 
 def main() -> None:
-    pen = Turtle()
-
-    colors: list = ["red", "green", "blue", "black"]
-
     screen = Screen()
-
-    screen.onkey(partial(pen.forward, 10), 'w')
-    screen.onkey(partial(pen.rt, 10), 'd')
-    screen.onkey(partial(pen.back, 10), 's')
-    screen.onkey(partial(pen.lt, 10), 'a')
-
-    screen.onkey(partial(clear_screen, pen), 'c')
-
-    screen.onkey(partial(switch_color, pen, colors), "space")
-
-    screen.listen()
+    screen.setup(width=600, height=400)
+    draw_finish_line(Turtle())
+    racers = create_racers()
+    guess = screen.textinput(title="Make your bet!", prompt="Which turtle do you support?: Enter a colour: ").lower()
+    race(racers)
+    print("Correct!" if winner[0] == guess else "Wrong!", f"{winner[0].capitalize()} is the winner!")
     screen.exitonclick()
 
 
