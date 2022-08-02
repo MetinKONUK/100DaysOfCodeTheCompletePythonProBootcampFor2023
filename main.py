25f857f2357491c8ed690d5cc71d54c07113611b
Metin KONUK <metinkonuk@hotmail.com>
2022-08-02 23:45:09 +0300

    executer file of snake game

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
 from time import sleep
-
+from food import Food
+from random import randint
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
+screen.title(titlestring="Snake Game")
+screen.tracer(n=0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, key="Up")
+screen.onkey(snake.down, key="Down")
+screen.onkey(snake.left, key="Left")
+screen.onkey(snake.right, key="Right")
 
-game_is_on = True
+game_is_on: bool = True
 
 while game_is_on:
     screen.update()
     sleep(0.05)
     snake.move()
-
+    if snake.head.distance(food) < 20:
+        food.create_food()
+        snake.add_segment()
 screen.exitonclick()
